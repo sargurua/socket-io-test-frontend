11d45562c77e16be7d29b45eb9baff7ba8bb57d3
Avinash Sarguru <avinashsarguru@gmail.com>
2020-02-11 17:18:00 -0600

    add socket client and made app component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   }
 
   render() {
-  ``  return (
+    return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
